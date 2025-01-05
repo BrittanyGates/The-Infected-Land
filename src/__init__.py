c90ce5a1e950535176259f56927bd5865ea16a22
Brittany Gates <bcgates82@gmail.com>
2025-01-05 14:34:11 -0600

    Uploaded file
    Uploaded file for the branch.

--- src/__init__.py
+++ src/__init__.py
@@ -9,8 +9,8 @@ from text_display_tools import *
 from the_ending import the_ending_choice, the_ending
 from villain import *
 
-__all__ = [after_beast_battle_story, after_human_battle_story, after_non_beast_battle_story, beast_battle, human_battle,
-           non_beast_battle, villain_battle, attack_or_block_successful, attack_or_block_unsuccessful, console,
-           hero_or_villain_defeated, notification, Hero, hero, improve_loadout, regain_health, story_intro,
-           attack_sleep_print, clear_screen, sleep_print, the_ending_choice, the_ending, Villain, beasts, non_beasts,
-           humans, random_beast, random_human, random_non_beast]
+__all__ = ["after_beast_battle_story", "after_human_battle_story", "after_non_beast_battle_story", "beast_battle",
+           "human_battle", "non_beast_battle", "villain_battle", "attack_or_block_successful",
+           "attack_or_block_unsuccessful", "console", "hero_or_villain_defeated", "notification", "Hero", "hero",
+           "attack_sleep_print", "clear_screen", "sleep_print", "Villain", "beasts", "non_beasts", "humans",
+           "random_beast", "random_human", "random_non_beast"]
