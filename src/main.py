b64c1592fea59d8f200cc2506eab194386293aec
Brittany Gates <bcgates82@gmail.com>
2025-11-14 05:46:41 -0600

    Refactored the input validation in the module.

--- src/main.py
+++ src/main.py
@@ -15,44 +15,40 @@ def game_menu() -> None:
     """Displays the game menu.
     :return: None
     """
-    clear_screen()
-    console.print()
-    console.print("THE INFECTED LAND", justify="center")
-    console.print()
-    console.print("0 - Quit Game", justify="center")
-    console.print("1 - New Game", justify="center")
-
-    try:
-        menu_choice: int = int(input("\nEnter your choice here: "))
-    except ValueError:
-        console.print()
-        console.print("!! This menu only accepts numbers !!", style=notification, justify="center")
-        console.print()
-        console.print("-- The game menu will reappear in a few seconds --", style=notification, justify="center")
-        sleep_print()
+    while True:
         clear_screen()
-        game_menu()
-    except (EOFError, KeyboardInterrupt):
         console.print()
+        console.print("THE INFECTED LAND", justify="center")
         console.print()
-        console.print(" -- Interrupt signal received --", style=notification, justify="center")
-        console.print()
-    else:
-        if menu_choice == 0:
+        console.print("0 - Quit Game", justify="center")
+        console.print("1 - New Game", justify="center")
+        try:
+            menu_choice: int = int(input("\nEnter your choice here: "))
+        except ValueError:
             console.print()
-            console.print("!! You quit the game !!", style=notification, justify="center")
+            console.print("!! This menu only accepts numbers !!", style=notification, justify="center")
+            console.print()
+            sleep_print()
+            continue
+        except (EOFError, KeyboardInterrupt):
+            console.print()
+            console.print(" -- Interrupt signal received --", style=notification)
             console.print()
             sys.exit()
-        elif menu_choice == 1:
-            story_intro()
         else:
-            console.print()
-            console.print("!! Enter either 0 or 1 !!", style=notification, justify="center")
-            console.print()
-            console.print(" -- The game menu will reappear in a few seconds -- ", style=notification, justify="center")
-            sleep_print()
-            clear_screen()
-            game_menu()
+            if menu_choice == 0:
+                console.print()
+                console.print("!! You quit the game !!", style=notification, justify="center")
+                console.print()
+                sys.exit()
+            elif menu_choice == 1:
+                story_intro()
+                break
+            else:
+                console.print()
+                console.print("!! Enter either 0 or 1 !!", style=notification, justify="center")
+                console.print()
+                sleep_print()
 
 
 if __name__ == "__main__":
