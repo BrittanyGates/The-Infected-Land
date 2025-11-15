a011369c2f9533b02154a80e74249fecd5f00f40
Brittany Gates <bcgates82@gmail.com>
2025-11-15 09:51:25 -0600

    Created the file to hold the game constants. Applied constants to the hero.py file.

---
+++ src/game_constants.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module contains the constants for the game."""
+
+# Hero
+HEALTH = 100
+WEAPON_TYPE = "Sword"
+BASE_SWORD_DAMAGE = 10
+ARMOR_TYPE = "Regular Armor"
+ARMOR_DEFENSE = 5
